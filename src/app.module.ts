560e362060ab070ef042a7d5f2669fb10f8b95a6
usamayousuf10p <usama.yousuf@10pearls.com>
2022-08-29 10:27:47 +0500

    mongodb package added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WeatherController } from './weather/weather.controller';
 import { WeatherService } from './weather/weather.service';
 import { HttpModule } from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [WeatherController],
   providers: [WeatherService],
 })
